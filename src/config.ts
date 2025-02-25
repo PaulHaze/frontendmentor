8f29404d358cf19ca57de939795b0c480f1132d2
PaulHaze <dev@artemist.com.au>
2025-02-25 12:11:30 +1100

    update the config

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 //!CHANGETHIS
-export const SITE_TITLE = 'Astro Playground';
+export const SITE_TITLE = 'Frontend Mentor Challenges';
 export const SITE_DESCRIPTION =
-	'A place for experiments, code practise and boundary pushing';
+	'A site for all my answers to the challenges provided by Frontend Mentor';
 export const GENERATE_SLUG_FROM_TITLE = true;
 export const TRANSITION_API = true;
