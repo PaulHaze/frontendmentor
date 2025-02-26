4424337fff00d380459ad8aca79efc353f234c01
PaulHaze <dev@artemist.com.au>
2025-02-26 17:09:27 +1100

    update the mock challenges for 2 and 3

--- src/data/challenges.ts
+++ src/data/challenges.ts
@@ -1,6 +1,6 @@
-import newbieImage001 from '/src/assets/images/newbie_001.jpg';
-import newbieImage002 from '/src/assets/images/newbie_002.jpg';
-import newbieImage003 from '/src/assets/images/newbie_003.jpg';
+import newbie_001 from '/src/assets/images/newbie_001.jpg';
+import newbie_002 from '/src/assets/images/newbie_002.jpg';
+import newbie_003 from '/src/assets/images/newbie_003.jpg';
 
 type ISODateString = `${number}-${number}-${number}`;
 
@@ -9,7 +9,7 @@ type Challenge = {
 	difficulty: 'newbie' | 'junior' | 'intermediate' | 'advanced' | 'guru';
 	slug: string;
 	title: string;
-	description: string;
+	challenge: string;
 	date: ISODateString;
 	thumbImg?: string;
 	coverImg: ImageMetadata;
@@ -25,11 +25,11 @@ export const challenges: Challenge[] = [
 		difficulty: 'newbie',
 		slug: 'recipe-page',
 		title: 'Recipe Page',
-		description:
+		challenge:
 			'Build out a responsive recipe page and get it looking as close to the design as possible.',
 		date: '2025-02-26',
 		thumbImg: 'src/assets/images/newbie_001.jpg',
-		coverImg: newbieImage001,
+		coverImg: newbie_001,
 		githubUrl: 'https://github.com/PaulHaze/frontendmentor',
 		figmaUrl: 'https://figma.com',
 		liveUrl: 'https://www.frontendmentor.io/challenges/recipe-page-KiTsR8QQKm',
@@ -39,33 +39,35 @@ export const challenges: Challenge[] = [
 	{
 		id: 'newbie_002',
 		difficulty: 'newbie',
-		slug: 'recipe-page',
-		title: 'Recipe Page',
-		description:
-			'Build out a responsive recipe page and get it looking as close to the design as possible.',
+		slug: 'social-links-profile',
+		title: 'Social Links Profile',
+		challenge:
+			'Build out a social link-sharing profile. It can even be personalized and used to share all of your social profiles.',
 		date: '2025-02-26',
 		thumbImg: 'src/assets/images/newbie_002.jpg',
-		coverImg: newbieImage002,
+		coverImg: newbie_002,
 		githubUrl: 'https://github.com/PaulHaze/frontendmentor',
 		figmaUrl: 'https://figma.com',
-		liveUrl: 'https://www.frontendmentor.io/challenges/recipe-page-KiTsR8QQKm',
+		liveUrl:
+			'https://www.frontendmentor.io/challenges/social-links-profile-UG32l9m6dQ',
 		projectUrl:
-			'https://www.frontendmentor.io/challenges/recipe-page-KiTsR8QQKm',
+			'https://www.frontendmentor.io/challenges/social-links-profile-UG32l9m6dQ',
 	},
 	{
 		id: 'newbie_003',
 		difficulty: 'newbie',
-		slug: 'recipe-page',
-		title: 'Recipe Page',
-		description:
-			'Build out a responsive recipe page and get it looking as close to the design as possible.',
+		slug: 'blog-preview-card',
+		title: 'Blog Preview Card',
+		challenge:
+			'An HTML & CSS-only challenge is a perfect project for beginners getting up to speed with HTML and CSS fundamentals, like HTML structure and the box model.',
 		date: '2025-02-26',
 		thumbImg: 'src/assets/images/newbie_003.jpg',
-		coverImg: newbieImage003,
+		coverImg: newbie_003,
 		githubUrl: 'https://github.com/PaulHaze/frontendmentor',
 		figmaUrl: 'https://figma.com',
-		liveUrl: 'https://www.frontendmentor.io/challenges/recipe-page-KiTsR8QQKm',
+		liveUrl:
+			'https://www.frontendmentor.io/challenges/blog-preview-card-ckPaj01IcS',
 		projectUrl:
-			'https://www.frontendmentor.io/challenges/recipe-page-KiTsR8QQKm',
+			'https://www.frontendmentor.io/challenges/blog-preview-card-ckPaj01IcS',
 	},
 ];
